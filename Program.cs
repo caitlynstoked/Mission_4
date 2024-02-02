0536b4b9fbad1e0cde567a136cfbab64a7bfb750
ccristil <ccristil@byu.edu>
2024-02-01 18:21:54 -0700

    check for player

--- Program.cs
+++ Program.cs
@@ -1,4 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.Metadata;
+
+int postion1 = 0;
+int postion2 = 0;
+string sign = "";
+int turns = 1;
+
 Console.WriteLine("Welcome to tic tac toe!");
+
+
+char[,] board = new char[3, 3];
+
+
+if (turns % 2 == 1)
+{
+    sign = "X";
+}
+else
+{
+    sign = "0"; 
+}
+
+
+
+//rest of the code
+
+
+
+
+
+
+
+
+//new value = sign
+//prints out board
+
+turns++;
+
+
+
+
+
 
 
