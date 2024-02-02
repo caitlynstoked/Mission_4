9a052baf9c650844252f9f9d10d29550c2554b66
ccristil <ccristil@byu.edu>
2024-02-01 19:29:07 -0700

    it works now

--- Program.cs
+++ Program.cs
@@ -2,34 +2,38 @@
 
 using System.Reflection.Metadata;
 
-int postion1 = 0;
-int postion2 = 0;
-string sign = "";
-int turns = 1;
 
-Console.WriteLine("Welcome to tic tac toe!");
+Console.WriteLine("Welcome to tic tac toe!\n");
 
 int position1 = 0;
 int position2 = 0;
+string sign = "";
+int turns = 1;
 List<string> board = new List<string>();
+List<string> chosenPositions = new List<string>(); // Keep track of chosen positions
 Boolean winner = false; //this will be set equal to the check for win function
+string playerChoice = "";
+string playerName = "";
 
 do 
 {
+    //assign a variable to the correct player
     if (turns % 2 == 1)
     {
         sign = "X";
+        playerName = "player 1";
     }
     else
     {
-        sign = "0"; 
+        sign = "O";
+        playerName = "player 2";
     }
 
 
-
+    // gather valid user inputs
     do
     {
-        Console.Write("Please enter an X value (0-2): ");
+        Console.Write($"Please enter an X value {playerName} (0-2): ");
         if (!int.TryParse(Console.ReadLine(), out position1) || position1 < 0 || position1 > 2)
         {
             Console.WriteLine("Please enter a valid numeric value between 0 and 2.");
@@ -38,20 +42,52 @@ do
 
     do
     {
-        Console.Write("Please enter a Y value (0-2): ");
+        Console.Write($"Please enter a Y value {playerName} (0-2): ");
         if (!int.TryParse(Console.ReadLine(), out position2) || position2 < 0 || position2 > 2)
         {
             Console.WriteLine("Please enter a valid numeric value between 0 and 2.");
         }
     } while (position2 < 0 || position2 > 2);
 
+    playerChoice = position1 + ", " + position2;
+
+    // check to make sure that there are no repeats
+    while (chosenPositions.Contains(playerChoice))
+    {
+        Console.WriteLine("Error: Position already chosen. Please choose a new position.");
+
+        // Ask for new coordinates
+        do
+        {
+            Console.Write($"Please enter an X value {playerName} (0-2): ");
+            if (!int.TryParse(Console.ReadLine(), out position1) || position1 < 0 || position1 > 2)
+            {
+                Console.WriteLine("Please enter a valid numeric value between 0 and 2.");
+            }
+        } while (position1 < 0 || position1 > 2);
+
+        do
+        {
+            Console.Write($"Please enter a Y {playerName} value (0-2): ");
+            if (!int.TryParse(Console.ReadLine(), out position2) || position2 < 0 || position2 > 2)
+            {
+                Console.WriteLine("Please enter a valid numeric value between 0 and 2.");
+            }
+        } while (position2 < 0 || position2 > 2);
+
+        playerChoice = position1 + ", " + position2;
+    }
 
 
 
+    chosenPositions.Add(playerChoice);
+    board.Add(playerChoice);
 
-    string PlayerChoice = position1 + ", " + position2;
+    foreach (string boardTurns in board)
+    {
+        Console.WriteLine(boardTurns);
 
-    board.Add(PlayerChoice);
+    }
 
     // call the print board function
     turns++;
@@ -63,7 +99,7 @@ do
 
 
     Console.WriteLine(sign);
-} while (winner = false);
+} while (winner == false);
 
 
 
